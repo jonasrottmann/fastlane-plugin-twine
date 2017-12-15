8c1481520feee194f7068b7f5ac9c77b2b608627
Jonas Rottmann <jonasrottmann@gmail.com>
2017-12-15 01:56:47 +0100

    Some tests

--- spec/empty_spec.rb
+++
@@ -1,6 +0,0 @@
-describe Fastlane::Actions::TwineValidateAction do
-  describe 'empty spec' do
-    it 'empty' do
-    end
-  end
-end
---
+++ spec/twine_validate_action_spec.rb
@@ -0,0 +1,20 @@
+describe Fastlane::Actions::TwineValidateAction do
+  describe 'Twine file validation' do
+    let (:path_to_nonextistent_config) { '' }
+    let (:path_to_empty_config) { './../spec/fixtures/empty_config.json' }
+
+    it 'Raise an error if config file was not found' do
+      expect do
+        result = Fastlane::FastFile.new.parse("lane :test do
+          twine_validate(configuration_path: '#{path_to_nonextistent_config}')
+        end").runner.execute(:test)
+      end.to raise_error("Config file  not found")
+    end
+
+    it 'Run validation on empty configuration' do
+      Fastlane::FastFile.new.parse("lane :test do
+        twine_validate(configuration_path: '#{path_to_empty_config}')
+      end").runner.execute(:test)
+    end
+  end
+end

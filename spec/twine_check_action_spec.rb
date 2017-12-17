d6b75d6469e984ed5cfb19f73e2bedf2e3385a67
Jonas Rottmann <jonasrottmann@gmail.com>
2017-12-17 02:04:54 +0100

    More tests

--- spec/twine_check_action_spec.rb
+++ spec/twine_check_action_spec.rb
@@ -2,6 +2,8 @@ describe Fastlane::Actions::TwineCheckAction do
   describe 'Twine file check' do
     let (:path_to_nonextistent_config) { '' }
     let (:path_to_empty_config) { './../spec/fixtures/empty_config.json' }
+    let (:config_empty_source_path) { './../spec/fixtures/config_empty_source_path.json' }
+    let (:config_hello_world) { './../spec/fixtures/config_hello_world.json' }
 
     it 'Raise an error if config file was not found' do
       expect do
@@ -11,9 +13,23 @@ describe Fastlane::Actions::TwineCheckAction do
       end.to raise_error("Config file  not found")
     end
 
+    it 'Raise an error if source file was not found' do
+      expect do
+        result = Fastlane::FastFile.new.parse("lane :test do
+          twine_validate(configuration_path: '#{config_empty_source_path}')
+        end").runner.execute(:test)
+      end.to raise_error("Source file  not found")
+    end
+
     it 'Run check on empty configuration' do
       Fastlane::FastFile.new.parse("lane :test do
         twine_check(configuration_path: '#{path_to_empty_config}')
+      end").runner.execute(:test)
+    end
+
+    it 'Check a localization file' do
+      Fastlane::FastFile.new.parse("lane :test do
+        twine_generate(configuration_path: '#{config_hello_world}')
       end").runner.execute(:test)
     end
   end
--- spec/twine_validate_action_spec.rb
+++ spec/twine_validate_action_spec.rb
@@ -2,6 +2,7 @@ describe Fastlane::Actions::TwineValidateAction do
   describe 'Twine file validation' do
     let (:path_to_nonextistent_config) { '' }
     let (:path_to_empty_config) { './../spec/fixtures/empty_config.json' }
+    let (:config_empty_source_path) { './../spec/fixtures/config_empty_source_path.json' }
 
     it 'Raise an error if config file was not found' do
       expect do
@@ -9,6 +10,14 @@ describe Fastlane::Actions::TwineValidateAction do
           twine_validate(configuration_path: '#{path_to_nonextistent_config}')
         end").runner.execute(:test)
       end.to raise_error("Config file  not found")
+    end
+
+    it 'Raise an error if source file was not found' do
+      expect do
+        result = Fastlane::FastFile.new.parse("lane :test do
+          twine_validate(configuration_path: '#{config_empty_source_path}')
+        end").runner.execute(:test)
+      end.to raise_error("Source file  not found")
     end
 
     it 'Run validation on empty configuration' do
